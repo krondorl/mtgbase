482a049ee17e2aa81e58faecc3818d985310f379
Adam Burucs <adam.burucs@gmail.com>
2026-05-17 20:25:09 +0200

    chore: update halfway to angular v21

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 /*!
  * Mtgbase
  *
@@ -11,5 +12,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch((err) => console.error(err));
